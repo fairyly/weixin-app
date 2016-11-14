bb430480c52bede9e197c4610754c5c2e7e7d64f
fairyly <498745097@qq.com>
2016-11-14 23:11:42 +0800

    add update

---
+++ xinhaibao(new2)/pages/index/index.js
@@ -0,0 +1,52 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+     imgUrls: [
+       {
+         src:'../../images/1.jpg',
+         id:1
+       },
+       {
+         src:'../../images/2.jpg',
+         id:2
+       },
+       {
+         src:'../../images/3.jpg',
+         id:3
+       }
+        
+      ],
+     indicatorDots: true,
+     autoplay: true,
+     interval: 5000,
+     duration: 1000
+  },
+  //事件处理函数
+  morepage: function() {
+    wx.navigateTo({
+      url: '../more/more'
+    })
+  },
+  edit:function(e){
+    var pageId=e.target.id;
+     console.log(pageId);
+     wx.navigateTo({
+      url: '../edit'+pageId+'/edit'+pageId
+    });
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+      }
+    })
+  }
+})
